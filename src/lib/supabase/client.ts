1f053143f908f9115c5c653e1a8630b3bf9debf1
vanhallen28 <virgiawanlistanto088@gmail.com>
2026-02-04 18:26:40 +0700

    Fix Bug

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,9 @@
 // src/lib/supabase/client.ts
 import { createBrowserClient } from "@supabase/ssr";
-import { environmentClient } from "@/configs/environment";
 
-export const supabase = createBrowserClient(
-  environmentClient.SUPABASE_URL,
-  environmentClient.SUPABASE_ANON_KEY
-);
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
